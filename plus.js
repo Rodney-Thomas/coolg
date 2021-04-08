50d544270d2ee96ff12cf002b2612dd709d7bd78
Rodney-Thomas <80913258+Rodney-Thomas@users.noreply.github.com>
2021-04-08 13:39:11 -0500

    Update plus.js

--- plus.js
+++ plus.js
@@ -5,11 +5,20 @@ function plus()
   function game()
   {
     text: (setText, color, font, xPos, yPos)=>{
-      if canvas.getContext) {
+      if (canvas.getContext) {
         const ctx = canvas.getContext('2d');
         ctx.fillStyle = color;
         ctx.font = font;
         ctx.fillText(setText, xPos, yPos);
+      }
+    },
+    entity: (type, color, xPos, yPos, width, height)=>{
+      if (canvas.getContext) {
+        const ctx = canvas.getContext('2d');
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.rect(20, 20, width, height);
+        ctx.stroke();
       }
     }
   }
