ec2f62d40d96da049149b8adebb4439b0739969b
Rodney-Thomas <80913258+Rodney-Thomas@users.noreply.github.com>
2021-04-08 13:47:49 -0500

    Update plus.js

--- plus.js
+++ plus.js
@@ -5,21 +5,22 @@ function plus()
   function game()
   {
     text: (setText, color, font, xPos, yPos)=>{
-      if (canvas.getContext) {
-        const ctx = canvas.getContext('2d');
-        ctx.fillStyle = color;
-        ctx.font = font;
-        ctx.fillText(setText, xPos, yPos);
-      }
+      const ctx = canvas.getContext('2d');
+      ctx.fillStyle = color;
+      ctx.font = font;
+      ctx.fillText(setText, xPos, yPos);
     },
-    entity: (type, color, xPos, yPos, width, height)=>{
-      if (canvas.getContext) {
-        const ctx = canvas.getContext('2d');
-        ctx.fillStyle = color;
-        ctx.beginPath();
-        ctx.rect(20, 20, width, height);
-        ctx.stroke();
-      }
+    entity: (color, xPos, yPos, width, height)=>{
+      const ctx = canvas.getContext('2d');
+      ctx.beginPath();
+      ctx.strokeStyle = color;
+      ctx.rect(20, 20, width, height);
+      ctx.stroke();
+    },
+    player: (color, startX, startY, width, height)=>{
+      const ctx = canvas.getContext'2d');
+      ctx.beginPath();
+      ctx.strokeStyle = color;
     }
   }
   
