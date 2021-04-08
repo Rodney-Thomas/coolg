1de6dc05cf5e64e94d9f3ddda7d6d5d32ff66605
Rodney-Thomas <80913258+Rodney-Thomas@users.noreply.github.com>
2021-04-08 13:23:27 -0500

    Update api.js

--- api.js
+++ api.js
@@ -3,12 +3,15 @@ function plusApi()
   title: (setTitle)=>{
     var titleDone = setTitle;
   },
+    
    description: (setDesc)=>{
      var descDone = setDesc;
    },
+     
    version: (setVer)=>{
      var verDone = setVer;
    },
+     
    start: (setDiv)=>{
      var divDone = setDiv;
    }
